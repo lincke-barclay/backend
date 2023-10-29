0d3fb0f4402582aeb886ca52680e22b7a2591692
Theo Lincke <lincketheo.dev@gmail.com>
2023-10-28 20:51:50 -0400

    adds secrets

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,14 +2,19 @@ import {Pool} from 'pg';
 
 const pool = new Pool({
     host: 'localhost',
-    user: 'theolincke',
-    database: 'events',
-    password: 'postgres',
+    user: process.env.POSTGRES_USERNAME,
+    database: process.env.POSTGRES_DB_NAME,
+    password: process.env.POSTGRES_PASSWORD,
     port: 5432,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
 });
+
+// Test connection fail
+pool.query("SELECT 1").then(() => {
+    console.log("Connection worked")
+})
 
 pool.on('connect', (client) => {
     console.log("Connected to Database !")
